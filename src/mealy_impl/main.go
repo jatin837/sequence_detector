26d7de2f4b655cc81d037d0edfe0cdfcfd9bfef5
dj4t9n <pandeyjatin47@gmail.com>
2021-05-03 14:56:19 +0530

    yeah, it's on for mealy impl ------------TODO------------   - do this for moores impl

--- src/mealy_impl/main.go
+++ src/mealy_impl/main.go
@@ -3,27 +3,95 @@ package main
 import (
   "github.com/pelletier/go-toml"
   "fmt"
+  "time"
 )
 
 var seq_file string = "/home/j43/dev/verilog/sequence_detector/seq.toml"
 var data, _ = toml.LoadFile(seq_file)
 var inputs = data.GetArray("sequence").([]int64)
 
-type state struct{
-	current uint8
-	previous uint8
-	input uint8
-	output uint8
+type state struct {
+  current uint8
+  input int64
+  next uint8
+  output int64
 }
   
-func (s *state) set_next_state(input int64) {
-		
+func (s *state) init_machine() {
+  s.current = 'a'
+  s.input = inputs[0]
+  s.set_next_state() // impl a function to set next state
+  s.set_output()
+}
+
+func (s *state) set_next_state() {
+  var res uint8 
+  switch s.current {
+    case 'a':
+      if s.input == 0 {
+        res = 'a'
+      } else{
+        res = 'b'
+      }
+    case 'b':
+      if s.input == 0 {
+        res = 'c'
+      } else{
+        res = 'b'
+      }
+    case 'c':
+      if s.input == 0 {
+        res = 'a'
+      } else{
+        res = 'd'
+      }
+    case 'd':
+      if s.input == 0 {
+        res = 'c'
+      } else{
+        res = 'b'
+      }
+  }
+  s.next = res
+}
+
+func (s *state) set_output() {
+  if s.current == 'd' && s.next == 'b' {
+    s.output = 1
+  } else {
+    s.output = 0
+  }
+}
+	
+func (s *state) go_forward(input int64){
+  s.input = input
+  s.current = s.next
+  s.set_next_state()
+  s.set_output()
 }
 
 func main() {
-  
-  for i := 0; i < len(inputs); i++{
-    fmt.Println(inputs[i])
+  var state state = state{}
+  sp := &state
+
+  fmt.Printf("input signals lenght = %d\n\n\n", len(inputs))
+  time.Sleep(3200*time.Millisecond)
+  fmt.Printf("------------------begin---------------\n\n\n")
+
+
+  sp.init_machine()
+
+  for i := 1; i < len(inputs); i++ {
+    time.Sleep(320*time.Millisecond)
+    sp.go_forward(inputs[i])
+    if state.output == 1 {
+      fmt.Printf("input = %d\t\t\t\t\t%s\n", inputs[i], "detected")
+      time.Sleep(3200*time.Millisecond)
+
+    } else {
+      fmt.Printf("input = %d\t\t\t\t\t%s\n", inputs[i], "waiting...")
+    }
   }
+  fmt.Printf("------------------end---------------\n\n\n")
 }
 
--- src/moore_impl/main.go
+++ src/moore_impl/main.go
@@ -4,26 +4,19 @@ import (
   "github.com/pelletier/go-toml"
   "fmt"
 )
-  
+
+var seq_file string = "/home/j43/dev/verilog/sequence_detector/seq.toml"
+var data, _ = toml.LoadFile(seq_file)
+var inputs = data.GetArray("sequence").([]int64)
+
+type state struct {
+  current uint8
+  input uint8
+  next uint8
+  output uint8
+}
+
 func main() {
-  var seq_file string = "/home/j43/dev/verilog/sequence_detector/seq.toml"
-   
-  data, _ := toml.LoadFile(seq_file)
-
-  inputs := data.GetArray("sequence").([]int64)
-  reset := data.Get("reset")
-  timeP := data.Get("time_period")
-  
-  fmt.Printf("type of inputs   ---------> %T\n", inputs)
-  fmt.Printf("type of reset    ---------> %T\n", reset)
-  fmt.Printf("type of timeP    ---------> %T\n", timeP)
-  fmt.Printf("%d is reset time, %d is time period\n\r", reset, timeP)
-  fmt.Println("input is :")
-  fmt.Println(inputs[4])
-  fmt.Println(len(inputs))
-
-  for i := 0; i < len(inputs); i++{
-    fmt.Println(inputs[i])
-  }
+ 
 }
 
