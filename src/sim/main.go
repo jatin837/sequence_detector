56a6ebc268a722c9e439f9fb7bf6cff2cb0f8746
dj4t9n <pandeyjatin47@gmail.com>
2021-05-03 12:51:01 +0530

    yeah now its solved
    HOW?
    ----
    what Get method returns is the interface
    in order to use that interface as a standard type, like string, []int64,
       you have to convert it
    
    now, my understanding of interface is not very good, so i need to spend
    some time there

--- src/sim/main.go
+++ src/sim/main.go
@@ -3,29 +3,26 @@ package main
 import (
   "github.com/pelletier/go-toml"
   "fmt"
-  "io/ioutil"
 )
   
-func check(e error) {
-  if e != nil {
-    panic(e)
-  }
-}
-
 func main() {
   var seq_file string = "/home/j43/dev/verilog/sequence_detector/seq.toml"
-  byte_data, err1 := ioutil.ReadFile(seq_file)
-  check(err1)
-  
-  string_data := string(byte_data)
-  data, _ := toml.Load(string_data)
+   
+  data, _ := toml.LoadFile(seq_file)
 
-  inputs := data.GetArray("sequence")
-  var b = []int64{1, 2, 3}
+  inputs := data.GetArray("sequence").([]int64)
+  reset := data.Get("reset")
+  timeP := data.Get("time_period")
   
-  fmt.Printf("type of inputs ---------> %T\n", inputs)
-  fmt.Printf("type of b ---------> %T\n", b)
-  fmt.Println(b)
-  fmt.Println(inputs)
+  fmt.Printf("type of inputs   ---------> %T\n", inputs)
+  fmt.Printf("type of reset    ---------> %T\n", reset)
+  fmt.Printf("type of timeP    ---------> %T\n", timeP)
+  fmt.Printf("%d is reset time, %d is time period\n\r", reset, timeP)
+  fmt.Println("input is :")
+  fmt.Println(inputs[4])
+  fmt.Println(len(inputs))
 
+  for i := 0; i < len(inputs); i++{
+    fmt.Println(inputs[i])
+  }
 }
